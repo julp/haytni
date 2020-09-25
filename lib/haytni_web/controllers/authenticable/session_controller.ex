059a5539cd9b91b32c98f0400f3c49dfc8b6fb53
julp <e2c2be7ed0f2f336@gmail.com>
2020-09-26 00:38:11 +0200

    fix HaytniWeb.Authenticable.SessionController treating the same way non-logged in and logged in users (on_logout callbacks might fail if current user is nil - and it is useless to call Haytni.logout in this case)

--- lib/haytni_web/controllers/authenticable/session_controller.ex
+++ lib/haytni_web/controllers/authenticable/session_controller.ex
@@ -2,7 +2,7 @@ defmodule HaytniWeb.Authenticable.SessionController do
   @moduledoc false
 
   use HaytniWeb, :controller
-  use HaytniWeb.Helpers, Haytni.AuthenticablePlugin
+  use HaytniWeb.Helpers, {Haytni.AuthenticablePlugin, :with_current_user}
 
   defp redirect(conn) do
     conn
@@ -16,11 +16,15 @@ defmodule HaytniWeb.Authenticable.SessionController do
     |> render("new.html")
   end
 
-  def new(conn, _params, _module, config) do
+  def new(conn, _params, nil, _module, config) do
     render_new(conn, Haytni.AuthenticablePlugin.session_changeset(config))
   end
 
-  def create(conn, %{"session" => session_params}, module, config) do
+  def new(conn, _params, _current_user, _module, _config) do
+    redirect(conn)
+  end
+
+  def create(conn, %{"session" => session_params}, nil, module, config) do
     case Haytni.AuthenticablePlugin.authenticate(conn, module, config, session_params) do
       {:ok, conn} ->
         conn
@@ -32,7 +36,15 @@ defmodule HaytniWeb.Authenticable.SessionController do
     end
   end
 
-  def delete(conn, _params, module, _config) do
+  def create(conn, _params, _current_user, _module, _config) do
+    redirect(conn)
+  end
+
+  def delete(conn, _params, nil, _module, _config) do
+    redirect(conn)
+  end
+
+  def delete(conn, _params, _current_user, module, _config) do
     conn
     |> Haytni.logout(module)
     |> redirect()
