458d397cfc30360eb2d5e09151df17ed757ead32
julp <e2c2be7ed0f2f336@gmail.com>
2021-07-27 23:37:09 +0200

    instructions to create test database

--- psql.sh
+++ psql.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+cat > /dev/null <<EOM
+su -l postgres -c /bin/sh <<EOF
+createuser -PE haytni
+createdb -O haytni haytni_test
+psql -c 'CREATE EXTENSION citext' -d haytni_test
+EOF
+EOM
+
 readonly PGPASS="${HOME}/.pgpass"
 readonly PATTERN='*:5432:haytni_test:haytni:haytni'
 if [ ! -f "${PGPASS}" ] || ! grep -qF "${PATTERN}" "${PGPASS}"; then
+	oldumask=`umask`
+	umask 0177
 	echo "${PATTERN}" >> "${PGPASS}"
+	umask $oldumask
 fi
 psql -d haytni_test -U haytni
-
