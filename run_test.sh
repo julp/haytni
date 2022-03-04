e08e4d887b688aa73aef6fd41d886d7296a0b5db
julp <e2c2be7ed0f2f336@gmail.com>
2022-03-04 22:17:58 +0100

    this doesn't work so remove it

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,5 @@ readonly __DIR__=`cd $(dirname -- "${0}"); pwd -P`
 
 if [ `find "${__DIR__}/deps" -d 0 -type d -empty | wc -l` -eq 1 ]; then
     MIX_ENV=test mix deps.get
-    # TODO: it seems that `mix compile` doesn't compile (CMake?) NIF (expassword_bcrypt or expassword_argon2) but an "explicit" `mix deps.compile` does
-    MIX_ENV=test mix deps.compile
 fi
 mix test
